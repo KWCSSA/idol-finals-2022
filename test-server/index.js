1af672ae38fc164a68a8ceefda793a43cda1c539
BruceWang1030 <r395wang@uwaterloo.ca>
2022-11-17 02:25:54 -0500

    17 Nov 2022 02:25:54

--- test-server/index.js
+++ test-server/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://it-kwcssa:NqHdbozqpPY9Psrn@cluster0.33koo.mongodb.net/final"
-);
+mongoose.connect(`${process.env.MONGODB}/final`);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
