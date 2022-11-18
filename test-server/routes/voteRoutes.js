3db2d5a03124e19370fc1e817f60202e1186b528
BruceWang1030 <r395wang@uwaterloo.ca>
2022-11-18 11:38:27 -0500

    fix semiFinal1 bug

--- test-server/routes/voteRoutes.js
+++ test-server/routes/voteRoutes.js
@@ -73,7 +73,8 @@ router.post("/", bodyParser, async (req, res, next) => {
   console.log("3:candidatesNum: ", candidatesNum);
 
   // check if button is valid
-  if (!roundIdx || candidateIdx < 0 || candidateIdx >= candidatesNum) {
+  console.log(candidateIdx, candidatesNum);
+  if (candidateIdx < 0 || candidateIdx >= candidatesNum) {
     return res.status(400).send({ message: "FINAL_BUTTON" });
   }
 
